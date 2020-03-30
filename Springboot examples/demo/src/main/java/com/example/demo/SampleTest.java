941f8d85a531ea8ff6f9650920c7113e2b2c0b9d
naveenyalla22 <naveenyalla22@gmail.com>
2020-03-30 18:30:25 +0530

    new changes

--- Springboot examples/demo/src/main/java/com/example/demo/SampleTest.java
+++ Springboot examples/demo/src/main/java/com/example/demo/SampleTest.java
@@ -74,7 +74,7 @@ public class SampleTest
         return op;
     }
 
-     static List<Integer> gradingStudents(List<Integer> grades)
+    static List<Integer> gradingStudents(List<Integer> grades)
     {
         // Write your code here
         List<Integer> newGrades = new ArrayList<>();
@@ -96,4 +96,136 @@ public class SampleTest
         }
         return newGrades;
     }
+
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+    {
+        int fallenApples = 0;
+        int fallenOranges = 0;
+
+        for (int i = 0; i < apples.length; i++)
+        {
+            if (a + apples[i] >= s && a + apples[i] <= t)
+                fallenApples++;
+        }
+
+        for (int i = 0; i < oranges.length; i++)
+        {
+            if (b + oranges[i] >= s && b + oranges[i] <= t)
+                fallenOranges++;
+        }
+        System.out.println(fallenApples);
+        System.out.print(fallenOranges);
+    }
+
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2)
+    {
+        while (x1 <= x2)
+        {
+            if (x1 == x2)
+            {
+                return "YES";
+                //System.exit(0);
+            }
+            x1 += v1;
+            x2 += v2;
+        }
+        return "NO";
+    }
+
+
+//5,99,139
+//5,72,662
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int b)
+    {
+        /*
+         * Write your code here.
+         */
+        int max = 0;
+        for (int i = 0; i < keyboards.length; i++)
+        {
+
+            for (int j = 0; i < drives.length; j++)
+            {
+                int sum = keyboards[i] + drives[j];
+                if (sum < b && sum < max)
+                    max = sum;
+            }
+        }
+
+        return max == 0 ? -1 : max;
+
+    }
+
+
+    // Complete the cutTheSticks function below.
+    static int[] cutTheSticks(int[] arrq)
+    {
+        int arr[] = {5, 4, 4, 2, 2, 8};
+        int sum = 0;
+        int[] cuts = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+            sum = sum + arr[i];
+
+        int index = 0;
+        while (sum > 0)
+        {
+            int first = 0;
+
+            for (int i = 0; i < arr.length; i++)
+                if (arr[i] == 0) continue;
+                else
+                    first = arr[i] < arr[i++] ? arr[i] : arr[i++];
+
+            int cnt = 0;
+            for (int i = 0; i < arr.length; i++)
+                if (arr[i] > 0)
+                {
+                    cnt++;
+                    arr[i] = arr[i] - first;
+                }
+
+            cuts[index++] = cnt;
+            sum = 0;
+            for (int i = 0; i < arr.length; i++)
+                sum = sum + arr[i];
+        }
+        return arr;
+    }
+
+    public static void main(String args[])
+    {
+        //int[] cuts = new int[0];
+        List<Integer> sss = new ArrayList<>();
+        nonDivisibleSubset(1, sss);
+    }
+
+    public static int nonDivisibleSubset(int k, List<Integer> s)
+    {
+        // Write your code here
+        k = 7;
+        s.addAll(Arrays.asList(278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436));
+        TreeSet<Integer> numSet = new TreeSet<Integer>();
+        for (int i = 0; i < s.size(); i++)
+        {
+            //System.out.println("IIII index  " + i);
+            for (int j = i + 1; j < s.size(); j++)
+            {
+                int rem = (s.get(i) + s.get(j)) % k;
+                if (rem != 0)
+                {
+                    numSet.add(s.get(i));
+                    numSet.add(s.get(j));
+                    //System.out.println("(" + s.get(i) + "," + s.get(j) + ")");
+                    System.out.println(s.get(i) + "\t" + s.get(j));
+                }
+            }
+        }
+        //System.out.println(numSet);
+        return numSet.size();
+    }
+
+
 }
